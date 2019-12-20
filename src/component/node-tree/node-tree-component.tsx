621c91baf6098503fe5e3a0248ecb92bb555c051
zack-baker1994 <zack.baker@accesscfa.com>
2019-12-19 22:51:51 -0500

    add key to list

--- src/component/node-tree/node-tree-component.tsx
+++ src/component/node-tree/node-tree-component.tsx
@@ -23,7 +23,7 @@ class NodeTreeComponent extends React.Component<any,StateTypes>{
 
     render(){
         let nodeList = this.state.data.nodes.map((node: NodeModel) => {
-            return <NodeComponent {...node} />
+            return <NodeComponent key={node.id} {...node} />
         });
         return (
             <div className='container'>
